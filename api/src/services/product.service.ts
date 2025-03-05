45c957e952d5b419394b387fe53a092700c78461
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-03-05 15:39:55 +0530

    feat : Add Product DB services

--- api/src/services/product.service.ts
+++ api/src/services/product.service.ts
@@ -1,11 +1,56 @@
-export const AddProduct = () => {};
+import ProductModel from "../models/product.model";
 
-export const updateProduct = () => {};
+export const AddProduct = async (
+  data: {
+    title: string;
+    category: string;
+    description?: string;
+    price: number;
+  },
+  userId: {},
+  imageUrl: string
+) => {
+  return await ProductModel.create({
+    title: data.title,
+    image: imageUrl,
+    category: data.category,
+    description: data.description,
+    price: data.price,
+    createdBy: userId,
+  });
+};
 
-export const deleteProduct = () => {};
+export const updateProduct = async (
+  data: {
+    title: string;
+    category: string;
+    description?: string;
+    price: number;
+  },
+  productId: {},
+  imageUrl: string
+) => {
+  return await ProductModel.findByIdAndUpdate(productId, {
+    title: data.title,
+    image: imageUrl,
+    category: data.category,
+    description: data.description,
+    price: data.price,
+  });
+};
 
-export const fetchProduct = () => {};
+export const deleteProduct = async (productId: {}) => {
+  return await ProductModel.findByIdAndDelete(productId);
+};
 
-export const fetchAllProduct = () => {};
+export const fetchProduct = async (productId: {}) => {
+  return await ProductModel.findOne(productId);
+};
 
-export const fetchProductByCategory = () => {};
+export const fetchAllProduct = async () => {
+  return await ProductModel.find();
+};
+
+export const fetchProductByCategory = async (category: string) => {
+  return await ProductModel.find({ category });
+};
