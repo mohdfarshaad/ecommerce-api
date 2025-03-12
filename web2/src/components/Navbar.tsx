327152d49b71292b594e4624a53eff65bbfed321
Mohammed Farshad <imohammedfarshad@gmail.com>
2025-03-12 23:08:05 +0530

    Add Tailwind css

---
+++ web2/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+      <div>
+          <ul>
+              <li>
+                 Shop 
+              </li>
+              <li>
+                 Search 
+              </li>
+              <li>
+                 Login 
+              </li>
+          </ul>
+    </div>
+  )
+}
+
+export default Navbar
